44a969c00b46cc2a57b7f0a9b606ee49a969f3a5
Guillaume Plaideau <guillaume.plaideau@cpe.fr>
2020-03-18 19:27:51 +0100

    nettoyage bean donnees DB

--- src/main/java/com/mamie/backend/BackEndApplication.java
+++ src/main/java/com/mamie/backend/BackEndApplication.java
@@ -32,13 +32,12 @@ public class BackEndApplication {
 
             familleRepository.deleteAll();
 
-            Famille plaideau = new Famille("Plaideau",4);
             Famille bozon = new Famille("Bozon",2);
             Famille molinet = new Famille("Molinet",1);
 
-            Famille desSources = new Famille("des Sources", 5);
+            Famille plaideau = new Famille("Plaideau", 5);
 
-            List<Famille> team = Arrays.asList(plaideau, bozon, molinet, desSources);
+            List<Famille> team = Arrays.asList(plaideau, bozon, molinet);
 
             System.out.println("Before linking up with Neo4j...");
 
@@ -47,7 +46,6 @@ public class BackEndApplication {
             familleRepository.save(plaideau);
             familleRepository.save(bozon);
             familleRepository.save(molinet);
-            familleRepository.save(desSources);
 
             System.out.println("Lookup each person by name...");
             System.out.println("\t" + familleRepository.findAll());
@@ -55,86 +53,66 @@ public class BackEndApplication {
             personneRepository.deleteAll();
 
 
-            List<Famille> GPlaideau = new ArrayList<>();
-            GPlaideau.add(plaideau);
-            GPlaideau.add(bozon);
 
-            Personne plaideauA = new Personne("plaideau","alain","ap@gmail.com", "21/09/1996", SexeEnum.male,
-                    "Lyon", "France", "http://benjamin.molinet.free.fr/projetMamie/medias/ap@gmail.com.png","Dev",
-                    "Ingé", "Celib", 23, plaideau, 2.3488, 41.8534);
-            Personne plaideauM = new Personne("plaideau","murielle","mp@gmail.com", "21/09/1996", SexeEnum.female,
-                    "Lyon", "France", "http://benjamin.molinet.free.fr/projetMamie/medias/mp@gmail.com.png","Dev",
-                    "Ingé", "Celib", 23, plaideau, 5.3488, 48.8534);
-            List<Personne> parentsPlaideau = new ArrayList<>();
-            parentsPlaideau.add(plaideauA);
-            parentsPlaideau.add(plaideauM);
-            Personne plaideauE = new Personne("plaideau","eleonore","ep@gmail.com", "21/09/1996", SexeEnum.female,
-                    "Lyon", "France", "http://benjamin.molinet.free.fr/projetMamie/medias/ep@gmail.com.png","Dev",
-                    "Ingé", "Celib", 23, plaideau, 8.3488, 42.8534);
-            List<Personne> sibling = new ArrayList<>();
-            sibling.add(plaideauE);
 
             Personne bozonG = new Personne("bozon","guillaume","gbozon@gmail.com", "21/09/1996", SexeEnum.male,
                     "Lyon", "France", "http://benjamin.molinet.free.fr/projetMamie/medias/gbozon@gmail.com.png","Dev",
                     "Ingé", "Celib", 23, bozon, 15.3488, 48.8534);
-            Personne plaideauG = new Personne("plaideau","guillaume","plaideaug83170@gmail.com", "21/09/1996", SexeEnum.male,
-                    "Lyon", "France", "http://benjamin.molinet.free.fr/projetMamie/medias/plaideaug83170@gmail.com.png","Dev",
-                    "Ingé", "Celib", 23, GPlaideau,sibling,parentsPlaideau,bozonG, 2.3488, 48.8534);
 
 
 
             Personne molinetB = new Personne("molinet","benjamin","bmolinet@gmail.com", "21/09/1996", SexeEnum.male,
                     "Lyon", "France", "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/p720x720/64219237_2511438135566953_6095685978989527040_o.jpg?_nc_cat=106&_nc_ohc=BK4j5MPhWsoAX_dGLal&_nc_ht=scontent-cdt1-1.xx&_nc_tp=1002&oh=951f388a9b6c21fddddf59b578b294c9&oe=5ECFFC2F","Dev","Ingé", "Celib", 23, molinet, 2.3488, 48.8534);
 
-            // ISABELLE
+            // MURIELLE
 
-            List<Personne> siblingsIsabelle = new ArrayList<>();
-            List<Personne> parentsIsabelle = new ArrayList<>();
+            List<Personne> siblingsMurielle = new ArrayList<>();
+            List<Personne> parentsMurielle = new ArrayList<>();
 
-            List<Famille> listDesSources = new ArrayList<>();
-            listDesSources.add(desSources);
+            List<Famille> listPlaideau = new ArrayList<>();
+            listPlaideau.add(plaideau);
 
-            Personne isabelleDesSources = new Personne("des Sources", "Isabelle", "isa@mail.fr", "23/05/1983",
+            Personne muriellePlaideau = new Personne("Plaideau", "Murielle", "mp@mail.fr", "23/05/1983",
                     SexeEnum.female, "Lyon", "France", "https://www.lequimag.be/sites/default/files/styles/article_big/public/actualite/przewalskis-horse-1660918_960_720.jpg?itok=TAiKzjGg",
-                    "Fleuriste", "BAC", "Mariée", 37, listDesSources, siblingsIsabelle, parentsIsabelle,
+                    "Fleuriste", "BAC", "Mariée", 37, listPlaideau, siblingsMurielle, parentsMurielle,
                     2.3488, 48.8534);
 
 
-            // HORUS
+            // ALAIN
 
-            List<Personne> siblingsHorus = new ArrayList<>();
-            List<Personne> parentsHorus = new ArrayList<>();
+            List<Personne> siblingsAlain = new ArrayList<>();
+            List<Personne> parentsAlain = new ArrayList<>();
 
-            Personne horusDesSources = new Personne("des Sources", "Horus", "horus@mail.fr", "23/05/1983",
+            Personne alainPlaideau = new Personne("Plaideau", "Alain", "ap@mail.fr", "23/05/1983",
                     SexeEnum.male, "Lyon", "France", "https://www.creativelune.com/wp-content/uploads/2017/04/galerie_photo_cheval_soleil_1208x800_72_creative_lune-1.jpg",
-                    "Fleuriste", "BAC", "Marié", 37, listDesSources, siblingsHorus, parentsHorus, isabelleDesSources,
+                    "Fleuriste", "BAC", "Marié", 37, listPlaideau, siblingsAlain, parentsAlain, muriellePlaideau,
                     2.3488, 48.8534);
 
 
-            // JET
+            // GUILLAUME
 
-            List<Personne> siblingsJet = new ArrayList<>();
-            List<Personne> parentsJet = new ArrayList<>();
-            parentsJet.add(isabelleDesSources);
-            parentsJet.add(horusDesSources);
+            List<Personne> siblingsGuillaume = new ArrayList<>();
+            List<Personne> parentsGuillaume = new ArrayList<>();
+            parentsGuillaume.add(muriellePlaideau);
+            parentsGuillaume.add(alainPlaideau);
 
-            Personne jetDesSources = new Personne("des Sources", "Jet", "jet@mail.fr", "23/05/1983",
+            Personne guillaumePlaideau = new Personne("Plaideau", "Guillaume", "plaideaug83170@gmail.com", "23/05/1983",
                     SexeEnum.male, "Lyon", "France", "https://thumbs.dreamstime.com/b/galop-de-cheval-sur-une-herbe-verte-40391667.jpg",
-                    "Fleuriste", "BAC", "Marié", 37, listDesSources, siblingsJet, parentsJet,
+                    "Fleuriste", "BAC", "Marié", 37, listPlaideau, siblingsGuillaume, parentsGuillaume,
                     2.3488, 48.8534);
 
 
-            // MANDARINE
+            // ELEONORE
 
-            List<Personne> siblingsMandarine = new ArrayList<>();
-            siblingsMandarine.add(jetDesSources);
-            List<Personne> parentsMandarine = new ArrayList<>();
-            parentsMandarine.add(isabelleDesSources);
-            parentsMandarine.add(horusDesSources);
+            List<Personne> siblingsEleonore = new ArrayList<>();
+            siblingsEleonore.add(guillaumePlaideau);
+            List<Personne> parentsEleonore = new ArrayList<>();
+            parentsEleonore.add(muriellePlaideau);
+            parentsEleonore.add(alainPlaideau);
 
-            Personne mandarineDesSources = new Personne("des Sources", "Mandarine", "manda@mail.fr", "23/05/1983",
+            Personne eleonorePlaideau = new Personne("Plaideau", "Eleonore", "leo@mail.fr", "23/05/1983",
                     SexeEnum.female, "Lyon", "France", "https://www.clubcheval.net/wp-content/uploads/2019/03/1b6d474d60f95a815f7f792428907658-1.jpe",
-                    "Fleuriste", "BAC", "Célibataire", 37, listDesSources, siblingsMandarine, parentsMandarine,
+                    "Fleuriste", "BAC", "Célibataire", 37, listPlaideau, siblingsEleonore, parentsEleonore,
                     2.3488, 48.8534);
 
 
@@ -145,26 +123,25 @@ public class BackEndApplication {
 
             Personne stellaDesSources = new Personne("des Sources", "Stella", "stella@mail.fr", "23/05/1983",
                     SexeEnum.female, "Lyon", "France", "https://fotomelia.com/wp-content/uploads/edd/2015/03/cheval-blanc-photo-en-noir-et-blanc-1560x1040.jpg",
-                    "Fleuriste", "BAC", "Mariée", 37, listDesSources, siblingsStella, parentsStella, jetDesSources,
+                    "Fleuriste", "BAC", "Mariée", 37, listPlaideau, siblingsStella, parentsStella, guillaumePlaideau,
                     2.3488, 48.8534);
 
 
 
 
 
-            List<Personne> teamP = Arrays.asList(plaideauG, bozonG, molinetB, isabelleDesSources, horusDesSources, jetDesSources,mandarineDesSources,stellaDesSources);
+            List<Personne> teamP = Arrays.asList( bozonG, molinetB, muriellePlaideau, alainPlaideau, guillaumePlaideau,eleonorePlaideau,stellaDesSources);
 
             System.out.println("Before linking up with Neo4j...");
 
             teamP.stream().forEach(personne -> System.out.println("\t" + personne.toString()));
 
-            personneRepository.save(plaideauG);
             personneRepository.save(bozonG);
             personneRepository.save(molinetB);
-            personneRepository.save(isabelleDesSources);
-            personneRepository.save(jetDesSources);
-            personneRepository.save(mandarineDesSources);
-            personneRepository.save(horusDesSources);
+            personneRepository.save(muriellePlaideau);
+            personneRepository.save(guillaumePlaideau);
+            personneRepository.save(eleonorePlaideau);
+            personneRepository.save(alainPlaideau);
             personneRepository.save(stellaDesSources);
 
 
@@ -173,15 +150,13 @@ public class BackEndApplication {
 
             evenementRepository.deleteAll();
 
-            Evenement event = new Evenement("Mariage", "21/09/2027", "Chez Caro", "https://www.photo-up.fr/public/Medias/3-seances/mariage/vignettes/reportage-mariage.jpg", plaideau);
             Evenement event2 = new Evenement("Vacances", "21/09/2027", "Ski Week", "https://www.djuringa-juniors.fr/media/cache/Sejours_Colonie/Hiver/2018/p/e/permiere-colonie-de-vacances-ski_375x281.jpg", bozon);
             Evenement event3 = new Evenement("Rattrapage", "21/09/2027", "A CPE", "http://www.retraite-et-travail.com/wp-content/uploads/2019/02/403890.jpg", molinet);
 
             Evenement event4 = new Evenement("Confinement", "21/09/2027", "Chez soi",
-                    "https://pbs.twimg.com/profile_images/1239275350292643845/GveP7naw_400x400.jpg", desSources);
+                    "https://pbs.twimg.com/profile_images/1239275350292643845/GveP7naw_400x400.jpg", plaideau);
 
 
-            evenementRepository.save(event);
             evenementRepository.save(event2);
             evenementRepository.save(event3);
             evenementRepository.save(event4);
